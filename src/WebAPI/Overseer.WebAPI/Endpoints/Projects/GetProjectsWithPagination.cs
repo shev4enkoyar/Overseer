ddb7b3f176766a384a7f75c81ff1ec3edb5a4b48
shev4enkoyar <schev4enko3@gmail.com>
2025-01-22 06:59:19 +0300

    :art: Added descriptions and some important tags to get projects endpoint

--- src/WebAPI/Overseer.WebAPI/Endpoints/Projects/GetProjectsWithPagination.cs
+++ src/WebAPI/Overseer.WebAPI/Endpoints/Projects/GetProjectsWithPagination.cs
@@ -1,5 +1,7 @@
+using System.ComponentModel;
 using MediatR;
 using Overseer.WebAPI.Application.Projects.Queries.GetProjectsWithPagination;
+using Overseer.WebAPI.Domain.Abstractions;
 using Overseer.WebAPI.Infrastructure;
 
 namespace Overseer.WebAPI.Endpoints.Projects;
@@ -8,10 +10,23 @@ public class GetProjectsWithPagination : IEndpoint
 {
     public static void MapEndpoint(RouteGroupBuilder routeGroupBuilder)
     {
-        routeGroupBuilder.MapGet("", async (ISender sender, IApiErrorHandler errorHandler,int pageNumber = 1, int pageSize = 10) =>
-        {
-            var response = await sender.Send(new GetProjectsWithPaginationQuery(pageNumber, pageSize));
-            return response.Match(Left: errorHandler.Handle, Right: Results.Ok);
-        });
+        routeGroupBuilder.MapGet("", async (
+                ISender sender,
+                IApiErrorHandler errorHandler,
+                [Description("The page number to retrieve. Must be greater than or equal to 1.")]
+                int pageNumber = 1,
+                [Description("The number of projects to include per page. Must be greater than or equal to 1.")]
+                int pageSize = 10) =>
+            {
+                var response = await sender.Send(new GetProjectsWithPaginationQuery(pageNumber, pageSize));
+                return response.Match(Left: errorHandler.Handle, Right: Results.Ok);
+            })
+            .WithSummary("Get projects")
+            .WithDescription(
+                "Returns a paginated list of project summaries, allowing the client to specify the page number and page size. " +
+                "If the request is successful, a paginated list of projects is returned. " +
+                "In case of an error (e.g., invalid parameters), a problem response is returned.")
+            .Produces<PaginatedList<ProjectBriefDto>>()
+            .ProducesValidationProblem();
     }
 }
